2520be4ffe52cd1077a8ccfb3fbbd57fe1ce943f
Martin Tesar <matty.tesar@gmail.com>
2020-07-08 08:54:30 +0200

    moved default filename value to Dropdown to prevent unwanted downloadable links

--- src/AddToCalendar.tsx
+++ src/AddToCalendar.tsx
@@ -59,8 +59,8 @@ type CalendarProps = {
 };
 
 const Calendar = React.forwardRef<CalendarRef, CalendarProps>(
-  ({ children, filename = 'download', href }, ref) => (
-    <a ref={ref} download={filename} href={href} target="_blank" rel="noopener noreferrer">
+  ({ children, filename, href }, ref) => (
+    <a ref={ref} download={Boolean(filename) ? filename : false} href={href} target="_blank" rel="noopener noreferrer">
       {children}
     </a>
   )
@@ -72,7 +72,7 @@ type DropdownProps = {
   filename?: string;
 };
 
-const Dropdown: React.FC<DropdownProps> = ({ onToggle, urls, filename }) => {
+const Dropdown: React.FC<DropdownProps> = ({ onToggle, urls, filename = 'download' }) => {
   const ref = useAutoFocus() as React.RefObject<HTMLAnchorElement>;
   const onKeyDown = (event: React.KeyboardEvent) => {
     if (event.key === "Escape") {
