cc7b778c3590f03732176e352fd0c411dc258147
Tony Ntasioudis <antonios.ntasioudis@ascential.com>
2021-01-28 11:07:09 +0000

    not needed function

--- src/__tests__/AddToCalendar.test.tsx
+++ src/__tests__/AddToCalendar.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { render, fireEvent, findAllByTestId } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import AddToCalendar from "../AddToCalendar";
 
