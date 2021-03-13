57de7f1a2cb0bf07a3cb75dfd12154daa41a009a
sundarcodes <sundar82@gmail.com>
2021-03-13 13:44:40 +0530

    Why: The need for doing the commit was
    * to have the parent/consumer of this AddToComponent library
    notified when the mouse click happens anywhere outside calendar button.
    
    This commit addresses the need by:
    
    * Add a callback to be passed as a props from the parent component
    * Adding necessary tests

--- src/AddToCalendar.tsx
+++ src/AddToCalendar.tsx
@@ -1,52 +1,52 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
-import makeUrls, { CalendarEvent } from "./makeUrls";
+import makeUrls, { CalendarEvent } from './makeUrls';
 
 type CalendarURLs = ReturnType<typeof makeUrls>;
 
 const useAutoFocus = () => {
   const elementRef = useRef<HTMLElement>(null);
 
-  useEffect(
-    () => {
-      const previous = document.activeElement;
-      const element = elementRef.current;
+  useEffect(() => {
+    const previous = document.activeElement;
+    const element = elementRef.current;
 
-      if (element) {
-        element.focus();
-      }
+    if (element) {
+      element.focus();
+    }
 
-      if (previous instanceof HTMLElement) {
-        return () => previous.focus();
-      }
+    if (previous instanceof HTMLElement) {
+      return () => previous.focus();
+    }
 
-      return undefined;
-    },
-    []
-  );
+    return undefined;
+  }, []);
 
   return elementRef;
 };
 
 type OpenStateToggle = (event?: React.MouseEvent) => void;
 
-const useOpenState = (initialOpen: boolean): [boolean, OpenStateToggle] => {
+const useOpenState = (
+  initialOpen: boolean,
+  onCalendarClose: Function
+): [boolean, OpenStateToggle] => {
   const [open, setOpen] = useState<boolean>(initialOpen);
-  const onToggle = () => setOpen(current => !current);
+  const onToggle = () => setOpen((current) => !current);
 
-  useEffect(
-    () => {
-      if (open) {
-        const onClose = () => setOpen(false);
-        document.addEventListener("click", onClose);
+  useEffect(() => {
+    if (open) {
+      const onClose = () => {
+        setOpen(false);
+        onCalendarClose();
+      };
+      document.addEventListener('click', onClose);
 
-        return () => document.removeEventListener("click", onClose);
-      }
+      return () => document.removeEventListener('click', onClose);
+    }
 
-      return undefined;
-    },
-    [open, setOpen]
-  );
+    return undefined;
+  }, [open, setOpen]);
 
   return [open, onToggle];
 };
@@ -60,7 +60,13 @@ type CalendarProps = {
 
 const Calendar = React.forwardRef<CalendarRef, CalendarProps>(
   ({ children, filename = false, href }, ref) => (
-    <a ref={ref} download={filename} href={href} target="_blank" rel="noopener noreferrer">
+    <a
+      ref={ref}
+      download={filename}
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       {children}
     </a>
   )
@@ -75,28 +81,26 @@ type DropdownProps = {
 const Dropdown: React.FC<DropdownProps> = ({ filename, onToggle, urls }) => {
   const ref = useAutoFocus() as React.RefObject<HTMLAnchorElement>;
   const onKeyDown = (event: React.KeyboardEvent) => {
-    if (event.key === "Escape") {
+    if (event.key === 'Escape') {
       onToggle();
     }
   };
 
   return (
-    <div className="chq-atc--dropdown" onKeyDown={onKeyDown} role="presentation">
+    <div
+      className="chq-atc--dropdown"
+      onKeyDown={onKeyDown}
+      role="presentation"
+    >
       <Calendar href={urls.ics} filename={filename} ref={ref}>
         Apple Calendar
       </Calendar>
-      <Calendar href={urls.google}>
-        Google
-      </Calendar>
+      <Calendar href={urls.google}>Google</Calendar>
       <Calendar href={urls.ics} filename={filename}>
         Outlook
       </Calendar>
-      <Calendar href={urls.outlook}>
-        Outlook Web App
-      </Calendar>
-      <Calendar href={urls.yahoo}>
-        Yahoo
-      </Calendar>
+      <Calendar href={urls.outlook}>Outlook Web App</Calendar>
+      <Calendar href={urls.yahoo}>Yahoo</Calendar>
     </div>
   );
 };
@@ -104,16 +108,21 @@ const Dropdown: React.FC<DropdownProps> = ({ filename, onToggle, urls }) => {
 type AddToCalendarProps = {
   event: CalendarEvent;
   open?: boolean;
+  onCalendarClose?: Function;
   filename?: string;
 };
 
 const AddToCalendar: React.FC<AddToCalendarProps> = ({
-  children = "Add to My Calendar",
+  children = 'Add to My Calendar',
   event,
-  filename = "download",
-  open: initialOpen = false
+  filename = 'download',
+  onCalendarClose,
+  open: initialOpen = false,
 }) => {
-  const [open, onToggle] = useOpenState(initialOpen);
+  const [open, onToggle] = useOpenState(
+    initialOpen,
+    onCalendarClose || (() => {})
+  );
   const urls = useMemo<CalendarURLs>(() => makeUrls(event), [event]);
 
   return (
@@ -122,8 +131,7 @@ const AddToCalendar: React.FC<AddToCalendarProps> = ({
         <button type="button" className="chq-atc--button" onClick={onToggle}>
           <svg width="20px" height="20px" viewBox="0 0 1024 1024">
             <path d="M704 192v-64h-32v64h-320v-64h-32v64h-192v704h768v-704h-192z M864 864h-704v-480h704v480z M864 352h-704v-128h160v64h32v-64h320v64h32v-64h160v128z" />
-          </svg>
-          {" "}
+          </svg>{' '}
           {children}
         </button>
       )}
--- src/__tests__/AddToCalendar.test.tsx
+++ src/__tests__/AddToCalendar.test.tsx
@@ -1,49 +1,54 @@
-import * as React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import * as React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 
-import AddToCalendar from "../AddToCalendar";
+import AddToCalendar from '../AddToCalendar';
 
 const mockEvent = {
-  name: "Happy Hour",
+  name: 'Happy Hour',
   details: "Let's go after work",
-  location: "Boston, MA",
-  startsAt: "2018-12-06T17:00:00-05:00",
-  endsAt: "2018-12-06T18:00:00-05:00"
+  location: 'Boston, MA',
+  startsAt: '2018-12-06T17:00:00-05:00',
+  endsAt: '2018-12-06T18:00:00-05:00',
 };
 
-const getDropped = (container: HTMLElement) => (
-  container.querySelector(".chq-atc--dropdown")
-);
+const getDropped = (container: HTMLElement) =>
+  container.querySelector('.chq-atc--dropdown');
 
-test("allows initial open to be set", () => {
+test('allows initial open to be set', () => {
   const { container } = render(<AddToCalendar event={mockEvent} open />);
 
   expect(getDropped(container)).toBeTruthy();
 });
 
-test("allows clicking outside to close", () => {
+test('allows clicking outside to close', () => {
+  const onCalendarCloseMock = jest.fn();
   const { container, getByText } = render(
     <div>
-      <AddToCalendar event={mockEvent} open />
+      <AddToCalendar
+        event={mockEvent}
+        open={true}
+        onCalendarClose={onCalendarCloseMock}
+      />
       <button type="button">Click Me</button>
     </div>
   );
 
-  fireEvent.click(getByText("Click Me"));
+  fireEvent.click(getByText('Click Me'));
+  expect(onCalendarCloseMock).toHaveBeenCalled();
   expect(getDropped(container)).toBeFalsy();
 });
 
-test("allows clicking the button to toggle", () => {
+test('allows clicking the button to toggle', () => {
   const { container, getByText } = render(<AddToCalendar event={mockEvent} />);
 
-  fireEvent.click(getByText("Add to My Calendar"));
+  fireEvent.click(getByText('Add to My Calendar'));
   expect(getDropped(container)).toBeTruthy();
 
-  fireEvent.click(getByText("Add to My Calendar"));
+  fireEvent.click(getByText('Add to My Calendar'));
   expect(getDropped(container)).toBeFalsy();
 });
 
-test("ignores other updates when establishing listener", () => {
+test('ignores other updates when establishing listener', () => {
   const { container, rerender } = render(<AddToCalendar event={mockEvent} />);
 
   rerender(<AddToCalendar event={mockEvent} open />);
@@ -51,26 +56,28 @@ test("ignores other updates when establishing listener", () => {
   expect(getDropped(container)).toBeFalsy();
 });
 
-test("makes expected links", () => {
+test('makes expected links', () => {
   const { container } = render(<AddToCalendar event={mockEvent} open />);
-  const hrefs = Array.from(container.querySelectorAll("a")).map(node => node.href).sort();
+  const hrefs = Array.from(container.querySelectorAll('a'))
+    .map((node) => node.href)
+    .sort();
 
   const name = encodeURIComponent(mockEvent.name);
-  const startTime = "20181206T220000Z";
-  const startTimeOutlook = "2018-12-06T17%3A00%3A00-05%3A00";
+  const startTime = '20181206T220000Z';
+  const startTimeOutlook = '2018-12-06T17%3A00%3A00-05%3A00';
 
-  const [,, google, yahoo, outlook] = hrefs;
+  const [, , google, yahoo, outlook] = hrefs;
 
-  hrefs.forEach(href => {
+  hrefs.forEach((href) => {
     expect(href).toContain(name);
     const expectedStartTime = href === outlook ? startTimeOutlook : startTime;
     expect(href).toContain(expectedStartTime);
   });
 
-  expect(google).toContain("https://calendar.google.com");
+  expect(google).toContain('https://calendar.google.com');
 
-  expect(yahoo).toContain("https://calendar.yahoo.com");
-  expect(yahoo).toContain(encodeURIComponent("0100"));
+  expect(yahoo).toContain('https://calendar.yahoo.com');
+  expect(yahoo).toContain(encodeURIComponent('0100'));
 
-  expect(outlook).toContain("https://outlook.live.com");
+  expect(outlook).toContain('https://outlook.live.com');
 });
